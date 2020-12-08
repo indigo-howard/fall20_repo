9649afc1c19994eed703bc8993a3fbd5660120ad
indigo-howard <56738297+indigo-howard@users.noreply.github.com>
2020-12-07 21:59:20 -0800

    Website as of Progress Report 8

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,35 @@
+// connecting index page
 // get data
 const guideList = document.querySelector('.guides');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
 
+const setupUI = (user) => {
+  if (user) {
+    //account info
+    db.collection('users').doc(user.uid).get().then(doc =>
+      {
+        const html = `
+        <div>Logged in as ${user.email}</div>
+        <div>Your Bio : ${doc.data().bio}</div>
+        `;
+        accountDetails.innerHTML = html;
+      });
+   
+  // toggle logged in or out
+    loggedInLinks.forEach(item => item.style.display ='block');
+    loggedOutLinks.forEach(item => item.style.display ='none');
+    
+  }
+  else{
+    //hide account info
+    accountDetails.innerHTML = 'hide';
+    //hide links
+    loggedInLinks.forEach(item => item.style.display ='none');
+    loggedOutLinks.forEach(item => item.style.display ='block');
+  }
+}
 //setup guides
 const setupGuides = (data)  => {
   if(data.length) {
